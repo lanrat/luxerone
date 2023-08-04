ec2b11bf7e078775a0ffcb6ca7fba473852e868e
Patrick Shinn <16513741+shinn5112@users.noreply.github.com>
2023-08-03 22:13:18 -0400

    Started working on breaking things out into classes instead of working with the raw dictionaries.

--- luxerone.py
+++ luxerone.py
@@ -1,32 +1,35 @@
-import urllib.request, urllib.parse
 import json
+import urllib.parse
+import urllib.request
 import uuid
 
-API_BASE = "https://resident-api.luxerone.com/resident_api/v1"
-
-default_headers = {
+_API_BASE = "https://resident-api.luxerone.com/resident_api/v1"
+_DEFAULT_HEADERS = {
     "content-type": "application/x-www-form-urlencoded",
     "accept": "application/json, text/plain, */*",
     "User-Agent": "okhttp/3.12.1",
 }
 
-def gen_uuid():
-    """ returns a 64 bit uuid as a hex string for new clients """
-    id = uuid.uuid4().int & (1<<64)-1
-    return hex(id)[2:]
+
+def _gen_uuid() -> str:
+    """
+    Generates a UUID.
+    :return: a 64 bit uuid as a hex string for new clients
+    """
+    generated_id = uuid.uuid4().int & (1 << 64) - 1
+    return hex(generated_id)[2:]
 
 
-def api_request(url, method="GET", token=None, data=None):
-    """ helper function for calling api endpoints
-
-    token:  the API token to add to the authorization header
-    data:   the message body for POST request that will be URL encoded
-
-    returns the returned json parsed as a dict
+def _api_request(url, method="GET", token=None, data=None):
+    """
+    Helper function for calling api endpoints
+    :param token:  the API token to add to the authorization header
+    :param data:   the message body for POST request that will be URL encoded
+    :return: the returned json parsed as a dict
     """
     if data:
         data = urllib.parse.urlencode(data).encode()
-    req = urllib.request.Request(url, method=method, data=data, headers=default_headers)
+    req = urllib.request.Request(url, method=method, data=data, headers=_DEFAULT_HEADERS)
     if token:
         req.add_header("authorization", "LuxerOneApi " + token)
 
@@ -36,121 +39,298 @@ def api_request(url, method="GET", token=None, data=None):
     return resp
 
 
-def login(username, password):
-    """ login to the service, returns a longterm token for later API calls """
-    url = API_BASE + "/auth/login"
-    id = gen_uuid()
-    data = {
-        "as": "token",
-        "expires": 1800,
-        "remember": True,
-        "uuid": id,
-        "username": username,
-        "password": password,
-    }
-    login_resp = api_request(url, method="POST", data=data)
+class Locker:
+    def __init__(self, package_data: dict):
+        """
+        Locker information class.
+        :param package_data: package data in json format from the API.
+        """
+        self.lockerId = None
+        self.lockerNumber = None
+        self.lockerTypeId = None
+        self.lockerType = None
+        for element in self.__dict__.keys():
+            try:
+                self.__dict__[element] = package_data[element]
+            except KeyError:
+                self.__dict__[element] = None
 
-    if "error" in login_resp:
-        raise Exception("login error: %s" % login_resp["error"])
-
-    # longterm login
-    url = API_BASE + "/auth/longterm"
-    data = {
-        "as": "token",
-        "expire": 18000000,
-    }
-    longterm_resp = api_request(url, method="POST", data=data, token=login_resp["data"]["token"])
-
-    if "error" in longterm_resp:
-        raise Exception("longterm login error: %s" % longterm_resp["error"])
-
-    return longterm_resp["data"]["token"]
-
-
-def pending(token):
-    """ returns a list of packages that are pending pickup """
-    url = API_BASE + "/deliveries/pendings"
-    resp = api_request(url, token=token)
-
-    if "error" in resp:
-        raise Exception("pending error: %s" % resp["error"])
-
-    return resp["data"]
+    def __str__(self) -> str:
+        """
+        Creates a string representation of the object.
+        :return: object string representation.
+        """
+        object_string = "["
+        counter = 0
+        dict_size = len(self.__dict__.items())
+        for key, value in self.__dict__.items():
+            object_string += f'{key}: {value}'
+            if counter != (dict_size - 1):
+                object_string += ", "
+            counter += 1
+        object_string += "]"
+        return object_string
 
 
-def user_info(token):
-    """ returns a dictionary of user info
-    keys from user info can be used in set_setting() to toggle settings
-    """
-    url = API_BASE + "/user/info"
-    resp = api_request(url, token=token)
+class Carrier:
+    def __init__(self, package_data: dict):
+        """
+        Carrier information class.
+        :param package_data: package data in json format from the API.
+        """
+        self.carrier = None
+        self.carrierLogo = None
+        self.trackingNumber = None
+        for element in self.__dict__.keys():
+            try:
+                self.__dict__[element] = package_data[element]
+            except KeyError:
+                self.__dict__[element] = None
 
-    if "error" in resp:
-        raise Exception("user_info error: %s" % resp["error"])
-
-    return resp["data"]
-
-
-def history(token):
-    """ returns a history of all packages received
-    includes pending packages
-    seems to be limited to last 50
-    """
-    url = API_BASE + "/deliveries/history"
-    resp = api_request(url, token=token)
-
-    if "error" in resp:
-        raise Exception("history error: %s" % resp["error"])
-
-    return resp["data"]
-
-
-def logout(token):
-    """ logout from the LuxerOne API
-    this function call appears to have no affect, likely broken server side
-    """
-    url = API_BASE + "/auth/logout"
-    data = {
-        "revoke": token,
-    }
-    resp = api_request(url, token=token, data=data, method="POST")
-
-    if "error" in resp:
-        raise Exception("logout error: %s" % resp["error"])
-
-    return resp["data"]
+    def __str__(self):
+        """
+        Creates a string representation of the object.
+        :return: object string representation.
+        """
+        object_string = "["
+        counter = 0
+        dict_size = len(self.__dict__.items())
+        for key, value in self.__dict__.items():
+            object_string += f'{key}: {value}'
+            if counter != (dict_size - 1):
+                object_string += ", "
+            counter += 1
+        object_string += "]"
+        return object_string
 
 
-def set_setting(token, key, value):
-    """ changes user settings.
-    keys are values from user_info()
-    not all options are changeable
-    """
-    url = API_BASE + "/user/settings"
-    # true/false are represented as 1/0
-    if value == True:
-        value = 1
-    if value == False:
-        value = 0
-    data = {
-        key: value,
-    }
-    resp = api_request(url, token=token, data=data, method="POST")
+class Location:
+    def __init__(self, package_data: dict):
+        """
+        Locker information class.
+        :param package_data: package data in json format from the API.
+        """
+        self.location = None
+        self.locationId = None
+        self.locationAddress = None
+        self.lockerLocation = None
+        self.timezoneOffset = None
+        for element in self.__dict__.keys():
+            try:
+                self.__dict__[element] = package_data[element]
+            except KeyError:
+                self.__dict__[element] = None
 
-    if "error" in resp:
-        raise Exception("set_setting error: %s" % resp["error"])
+    def __str__(self):
+        """
+        Creates a string representation of the object.
+        :return: object string representation.
+        """
+        object_string = "["
+        counter = 0
+        dict_size = len(self.__dict__.items())
+        for key, value in self.__dict__.items():
+            object_string += f'{key}: {value}'
+            if counter != (dict_size - 1):
+                object_string += ", "
+            counter += 1
+        object_string += "]"
+        return object_string
 
-    return resp["data"]
+
+class Package:
+    def __init__(self, package_data: dict):
+        """
+        Package information class.
+        :param package_data: package data in json format from the API.
+        """
+        self.id = None
+        self.deliveryTypeId = None
+        self.delivered = None
+        self.pickedup = None
+        self.holdUntil = None
+        self.accessCode = None
+        self.isPerishable = False
+        self.status = None
+        self.charge = None
+        self.pickupToken = None
+        self.labels = None
+        for element in self.__dict__.keys():
+            try:
+                self.__dict__[element] = package_data[element]
+            except KeyError:
+                self.__dict__[element] = None
+        self.carrier = Carrier(package_data)
+        self.locker = Locker(package_data)
+        self.location = Location(package_data)
+
+    def __str__(self):
+        """
+        Creates a string representation of the object.
+        :return: object string representation.
+        """
+        object_string = "["
+        counter = 0
+        dict_size = len(self.__dict__.items())
+        for key, value in self.__dict__.items():
+            object_string += f'{key}: {value}'
+            if counter != (dict_size - 1):
+                object_string += ", "
+            counter += 1
+        object_string += "]"
+        return object_string
 
 
-def print_package(data):
-    """ print some package information """
-    print("%s\t%s\t%s\t%s\t%s" % (data['id'], data['delivered'][:-6],data['carrier'],data['lockerNumber'],data['accessCode']))
+class HistoricalPackage(Package):
+    def __init__(self, package_data: dict):
+        """
+        Historical Package info class.
+        :param package_data: package data in json format from the API.
+        """
+        super().__init__(package_data)
+        try:
+            self.resident = package_data["resident"]
+        except KeyError:
+            self.resident = None
+        try:
+            self.signature = package_data["signature"]
+        except KeyError:
+            self.signature = None
+
+    def __str__(self):
+        """
+        Creates a string representation of the object.
+        :return: object string representation.
+        """
+        object_string = "["
+        counter = 0
+        dict_size = len(self.__dict__.items())
+        for key, value in self.__dict__.items():
+            object_string += f'{key}: {value}'
+            if counter != (dict_size - 1):
+                object_string += ", "
+            counter += 1
+        object_string += "]"
+        return object_string
 
 
-def print_packages(data, limit=0):
-    """ print a list of packages """
-    if limit != 0:
-        data = data[:limit]
-    for p in data:
-        print_package(p)
+class LuxerOneClient:
+    def __init__(self):
+        """
+        Unofficial LuxerOne Python client.
+        """
+        self._token = None
+
+    def login(self, username: str, password: str) -> None:
+        """
+
+        :param username:
+        :param password:
+        :return:
+        """
+        url = _API_BASE + "/auth/login"
+        generated_id = _gen_uuid()
+        data = {
+            "as": "token",
+            "expires": 1800,
+            "remember": True,
+            "uuid": generated_id,
+            "username": username,
+            "password": password,
+        }
+        login_resp = _api_request(url, method="POST", data=data)
+
+        if "error" in login_resp:
+            raise Exception("login error: %s" % login_resp["error"])
+
+        # longterm login
+        url = _API_BASE + "/auth/longterm"
+        data = {
+            "as": "token",
+            "expire": 18000000,
+        }
+        longterm_resp = _api_request(url, method="POST", data=data, token=login_resp["data"]["token"])
+
+        if "error" in longterm_resp:
+            raise Exception("longterm login error: %s" % longterm_resp["error"])
+
+        self._token = longterm_resp["data"]["token"]
+
+    def get_pending_packages(self):
+        """
+        Gets the list of current packages that have been delivered but not picked up.
+        :returns: a list of packages that are pending pickup
+        """
+        url = _API_BASE + "/deliveries/pendings"
+        resp = _api_request(url, token=self._token)
+
+        if "error" in resp:
+            raise Exception("pending error: %s" % resp["error"])
+        # TODO return pacakge objects
+        return resp["data"]
+
+    def get_user_info(self):
+        """
+        returns a dictionary of user info
+        keys from user info can be used in set_setting() to toggle settings
+        """
+        # TODO make a user info object
+        url = _API_BASE + "/user/info"
+        resp = _api_request(url, token=self._token)
+
+        if "error" in resp:
+            raise Exception("user_info error: %s" % resp["error"])
+
+        return resp["data"]
+
+    def get_package_history(self):
+        """ returns a history of all packages received
+        includes pending packages
+        seems to be limited to last 50
+        """
+        url = _API_BASE + "/deliveries/history"
+        resp = _api_request(url, token=self._token)
+
+        if "error" in resp:
+            raise Exception("history error: %s" % resp["error"])
+
+        # TODO return historical package objects
+        return resp["data"]
+
+    def logout(self):
+        """ logout from the LuxerOne API
+        this function call appears to have no affect, likely broken server side
+        """
+        url = _API_BASE + "/auth/logout"
+        data = {
+            "revoke": self._token,
+        }
+        resp = _api_request(url, token=self._token, data=data, method="POST")
+
+        if "error" in resp:
+            raise Exception("logout error: %s" % resp["error"])
+
+        return resp["data"]
+
+    def set_setting(self, key, value):
+        """
+        changes user settings.
+        keys are values from user_info()
+        not all options are changeable
+        """
+        # TODO figure out which settings can be set and create an object for it
+        url = _API_BASE + "/user/settings"
+        # true/false are represented as 1/0
+        if value:
+            value = 1
+        else:
+            value = 0
+        data = {
+            key: value,
+        }
+        resp = _api_request(url, token=self._token, data=data, method="POST")
+
+        if "error" in resp:
+            raise Exception("set_setting error: %s" % resp["error"])
+
+        return resp["data"]
